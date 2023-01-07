8f9f89afa9e77a27526ac2be1c3118b726df9470
ajeddin <abduljeddin@gmail.com>
2023-01-07 14:14:46 +0300

    typo

--- server/main.js
+++ server/main.js
@@ -9,7 +9,7 @@ const {
 seed,addQoute,getQoute,getAllQoutes,deleteQoute,getGIF
 } = require('./controller.js')
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../public'))
+    res.sendFile(path.join(__dirname,'../public'&& '../face-api.min.js'))
 })
 app.use(express.json())
 app.use(cors())
